042d9cb13c72b1341295337e75972b231d26d760
Huy Nguyen <duchuyn0411@gmail.com>
2024-06-21 03:08:22 -0700

    clean up

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import contactImg from "../assets/img/contact-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
@@ -7,23 +7,17 @@ export const Contact = () => {
   return (
     <section className="contact" id="connect">
       <Container>
-        <Row className="align-items-center">
-          <Col size={12} md={6}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us" />
-              }
-            </TrackVisibility>
-          </Col>
-          <Col size={12} md={6}>
+        <Row className="align-items-center justify-content-center">
+          <Col size={12} md={6} className="text-center"> {/* Centering the column content */}
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                  <h2 >Contact</h2>
+                  <h2>Contact</h2>
                   <h5>Email: <a href="mailto:duchuyn0411@gmail.com" style={{ color: "white", textDecoration: "none" }}>duchuyn0411@gmail.com</a></h5>
                   <h5>Linkedin: <a href="https://www.linkedin.com/in/huy-nguy3n/" style={{ color: "white", textDecoration: "none" }} target="_blank" rel="noopener noreferrer">linkedin.com/in/huy-nguy3n/</a></h5>
                   <h5>Github: <a href="https://github.com/huy-nguy3n" style={{ color: "white", textDecoration: "none" }} target="_blank" rel="noopener noreferrer">github.com/huy-nguy3n</a></h5>
-                </div>}
+                </div>
+              }
             </TrackVisibility>
           </Col>
         </Row>
@@ -31,3 +25,5 @@ export const Contact = () => {
     </section>
   )
 }
+
+export default Contact;
